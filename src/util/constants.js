817d6a999e4851f616b1f637a15ef930ad857adb
Michael Nelson <michael.nelson@fulbrightmail.org>
2017-06-15 22:44:23 -0400

    add constants

---
+++ src/util/constants.js
@@ -0,0 +1,16 @@
+// @flow
+import * as colors from 'material-ui/styles/colors'
+
+export const appName = 'Transitional Care Analytics Dashboard'
+export const applicationJsonContentHeader = {
+  'Content-Type': 'application/json'
+}
+export const authEndpoint = 'http://localhost:5000/auth/user/'
+export const endpoint = 'http://localhost:5000'
+export const iconColor = colors.white
+export const primaryColor = colors.indigo300
+export const rootElement = document.getElementById('root')
+export const testCredentials = {
+  'email': 'michaelnet@michaelnet.biz',
+  'password': 'lolwut?'
+}
